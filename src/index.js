2df86679ae38871378314aea2b102243bc57ef0f
Alex Jongman <alex.jongman@hu.nl>
2022-02-17 09:52:29 +0100

    removed unused files

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import './view/components/coin-list-item.js';
 import './view/pages/home-page.js';
 import './view/pages/page-not-found.js';
 
-import './app.serviceworker.js';
 import './service/routing.service.js'
